46618d2e054244bd74d2aecbffe3444aaadcbdbe
g4b7i3l <gabiibagna@gmail.com>
2021-05-28 15:23:42 -0300

    levanta servidor

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require ("express"); // modulo de tercero - se invoca solo con el nombre
 
-const app = express ();
+const app = express (); // funcion de alto nivel llamada express
 
-app.listen(3000,() => console.log("Servidor funcionando en el puerto 3000")) 
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+const port = 3000
+
+app.listen(port,() => console.log("Servidor funcionando en el puerto "+ port )) // 3000 es el numero de puerto - seguido de un callback
+
