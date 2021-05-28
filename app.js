396fbf77cd8d6f7ef31447d1d0a257379babbd6c
g4b7i3l <gabiibagna@gmail.com>
2021-05-28 14:56:19 -0300

    mi primer commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require ("express"); // modulo de tercero - se invoca solo con el nombre
+
+const app = express ();
+
+app.listen(3000,() => console.log("Servidor funcionando en el puerto 3000")) 
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
